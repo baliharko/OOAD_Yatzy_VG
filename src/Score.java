56f22a947a012eae61934a26f418b0e09f28356f
Oscar Norman <oscar.norman95@gmail.com>
2020-11-24 14:56:39 +0100

    init

---
+++ src/Score.java
@@ -0,0 +1,15 @@
+import java.time.LocalDate;
+
+/**
+ * Created by Oscar Norman <br>
+ * Date: 2020-11-24   <br>
+ * Time: 14:42   <br>
+ * Project: JAVA20-OOAD-Projektarbete-Grupp-10 <br>
+ */
+public class Score {
+
+    public Score() {
+
+    }
+
+}
