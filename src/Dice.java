5bdae16d068bbb33d30bfa48f645a7178010b04c
Viktor Kodet <viktor.kodet@gmail.com>
2020-11-24 14:46:27 +0100

    dice

---
+++ src/Dice.java
@@ -0,0 +1,25 @@
+import java.util.Random;
+
+/**
+ * Created by Viktor Kodet <br>
+ * Date: 2020-11-24 <br>
+ * Time: 14:40 <br>
+ * Project: IntelliJ IDEA <br>
+ */
+public class Dice {
+    Random random = new Random();
+
+    private int value;
+
+    Dice(){
+        roll();
+    }
+
+    public void roll(){
+        value = random.nextInt(6) + 1;
+    }
+
+    public int getValue(){
+        return value;
+    }
+}
