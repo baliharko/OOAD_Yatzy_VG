650369bd8db1bae6cf5611244b407b0047c44d51
Oscar Norman <oscar.norman95@gmail.com>
2020-12-04 15:17:39 +0100

    merged from WIP and modified ScoreClass

--- src/Yatzy/Score.java
+++ src/Yatzy/Score.java
@@ -1,4 +1,36 @@
 package Yatzy;
 
+import java.time.LocalDate;
+
 public class Score {
+
+    //private String playerName;
+    private int score;
+    private LocalDate date;
+
+    public Score(int score) {
+        this.score = score;
+        this.date = LocalDate.now();
+
+        calculateScore();
+    }
+
+    public void calculateScore() {
+        if (this.score >= 42)
+            this.score += 30;
+    }
+
+    @Override
+    public String toString() {
+        return "Score{" +
+                "score=" + score +
+                ", date=" + date +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Score points = new Score(100);
+
+        System.out.println(points.toString());
+    }
 }
