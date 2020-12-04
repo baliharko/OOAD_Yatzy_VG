c6bf6c7e63c740c151e8faa6fe82daac735feb4a
Hanna Edlund <hanna@edlunds.biz>
2020-12-04 15:08:39 +0100

    Added Game

--- src/Yatzy/Game.java
+++ src/Yatzy/Game.java
@@ -1,5 +1,13 @@
 package Yatzy;
 
 public abstract class Game {
+    private String player;
+    private int currentScore;
+    Die[] dice = new Die[5];
+    private int currentThrow;
+    Database database;
 
+    public void rollDice(){
+
+    }
 }
