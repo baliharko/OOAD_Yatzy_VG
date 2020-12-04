d08473903764eb197a4613bf9a3f7e2d6eb8b78e
baliharko <bali@fauxdelorean.com>
2020-12-04 14:59:17 +0100

    new database

--- src/Yatzy/Database.java
+++ src/Yatzy/Database.java
@@ -1,4 +1,37 @@
-package Yatzy;
+package Database;
+
+import Yatzy.Score;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Database {
+
+    private static Database single_instance = null;
+    private static final String FILEPATH = ""; // Temp
+    private List<Score> listOfScores;
+
+    private Database() {
+        this.listOfScores = new ArrayList<>();
+        Database.single_instance = this;
+        System.out.println("Database object created.");
+    }
+
+    public static Database getInstance() {
+        return single_instance == null ? new Database() : null;
+    }
+
+    public void addScore(Score score) {
+        // Sort before
+        this.listOfScores.add(score);
+    }
+
+    public void loadData() {
+        // Loads data from .ser file.
+    }
+
+    public void saveData() {
+        // Saves data to .ser file.
+    }
+
 }
