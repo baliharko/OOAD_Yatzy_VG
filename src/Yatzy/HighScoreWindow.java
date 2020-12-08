c0744684c68d10c4e064b276ffe8452255583b2f
Tomas Dahlander <tomas.dahlander@gmail.com>
2020-12-08 09:59:48 +0100

    Added ScrollPane and testmethod for adding names on Scoreboard

--- src/Yatzy/HighScoreWindow.java
+++ src/Yatzy/HighScoreWindow.java
@@ -5,7 +5,6 @@ import java.awt.*;
 
 public class HighScoreWindow extends JFrame {
 
-    private JPanel mainPanel = new JPanel();
     private JPanel topPanel = new JPanel();
     private JLabel header = new JLabel("Scoreboard");
     private JTextArea scoreArea = new JTextArea();
@@ -14,29 +13,36 @@ public class HighScoreWindow extends JFrame {
     public HighScoreWindow(){
         setUpScoreBoardLabel();
         setUpScoreBoard();
+        testMethodForAddingNamesToScoreboard("Orvar Karlsson",120);
         setUpJFrame();
     }
 
     public void setUpScoreBoardLabel(){
+        this.setLayout(new BorderLayout());
         header.setFont(new Font("SansSerif", Font.PLAIN,24));
         topPanel.add(header);
-        mainPanel.add(topPanel);
+        add(topPanel,BorderLayout.NORTH);
     }
 
     public void setUpScoreBoard(){
-        this.add(mainPanel);
+        sp.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         scoreArea.setEditable(false);
-        scoreArea.setForeground(Color.BLACK);
-        scoreArea.setPreferredSize(new Dimension(280,430));
+        sp.setPreferredSize(new Dimension(260,390));
         scoreArea.setFont(new Font("SansSerif", Font.PLAIN,14));
-        scoreArea.append("1. First score (test)\n"); // Test
-        scoreArea.append("2. Second score (test)\n"); // Test
-        mainPanel.add(sp);
+        add(sp,BorderLayout.CENTER);
+    }
+
+    public void testMethodForAddingNamesToScoreboard(String name, int loops){
+        for(int i = 1; i < loops; i++){
+            String temp = String.format("%3d. %s",i,name);
+            scoreArea.append(temp + "\n");
+        }
     }
 
     public void setUpJFrame(){
         this.setLocation(1000,150);
         this.setSize(300,500);
+        this.setResizable(false);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setVisible(true);
     }
