442e11d95399238d1628ed2a0a88cb1c8da24135
baliharko <bali@fauxdelorean.com>
2020-12-08 09:52:37 +0100

    diceButtons color now show properly on macOS

--- src/Yatzy/Controller.java
+++ src/Yatzy/Controller.java
@@ -1,6 +1,7 @@
 package Yatzy;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Controller {
 
@@ -14,8 +15,8 @@ public class Controller {
         window.getYatzyPanel().roll.addActionListener(l -> {
             Die[] dice = game.rollDice();
             JToggleButton[] toggleButtons = getDiceButtons();
-            for (int i = 0; i < dice.length; i++){
-                if(!toggleButtons[i].isSelected()){
+            for (int i = 0; i < dice.length; i++) {
+                if (!toggleButtons[i].isSelected()) {
                     toggleButtons[i].setText("" + dice[i].getValue());
                 }
             }
@@ -24,22 +25,29 @@ public class Controller {
         window.getYatzyPanel().show.addActionListener(l -> {
             new HighScoreWindow();
         });
+
+        for (JToggleButton diceButton : window.getYatzyPanel().diceButtons) {
+            diceButton.addActionListener(l -> {
+                if (diceButton.isSelected())
+                    diceButton.setBackground(new Color(184,207,229));
+                else
+                    diceButton.setBackground(game.gameColor);
+            });
+        }
     }
 
-    public void setUpStartButtonListener(){
+    public void setUpStartButtonListener() {
         window.getStartPanel().getStartGameButton().addActionListener(l -> {
-            if (window.getStartPanel().getNotRankedGameButton().isSelected()){
+            if (window.getStartPanel().getNotRankedGameButton().isSelected()) {
                 window.setTitle("Just playing for fun... loser");
                 startUnrankedGame();
                 window.changePanelTo(window.getYatzyPanel());
-            }
-            else if (window.getStartPanel().getRankedGameButton().isSelected()){
-                if (window.getStartPanel().getNameField().getText().length() > 2){
+            } else if (window.getStartPanel().getRankedGameButton().isSelected()) {
+                if (window.getStartPanel().getNameField().getText().length() > 2) {
                     this.window.setTitle("Name: " + window.getStartPanel().getNameField().getText());
                     startRankedGame();
                     window.changePanelTo(window.getYatzyPanel());
-                }
-                else System.out.println("Du måste ange ett namn/alias med minst 3 tecken.");
+                } else System.out.println("Du måste ange ett namn/alias med minst 3 tecken.");
             }
         });
     }
@@ -54,7 +62,7 @@ public class Controller {
         this.window.getYatzyPanel().setColor(game.getGameColor());
     }
 
-    public JToggleButton[] getDiceButtons(){
+    public JToggleButton[] getDiceButtons() {
         return window.getYatzyPanel().diceButtons;
     }
 }
